607a19f566e2685478518a3e6c2bfde04b7807bb
Ian Wrzesinski <ian@isuffix.com>
2025-08-13 22:07:35 -0400

    working_copy: Move `CheckoutOptions` to `LocalWcSettings`
    With `LocalWcSettings` we now have a better place to set the conflict marker
    styles which obviates the need for `CheckoutOptions` and lets us remove them
    from `SnapshotOptions`, improving ergonomics.

--- lib/src/local_working_copy.rs
+++ lib/src/local_working_copy.rs
@@ -112,7 +112,6 @@ use crate::settings::UserSettings;
 use crate::store::Store;
 use crate::tree::Tree;
 use crate::working_copy::CheckoutError;
-use crate::working_copy::CheckoutOptions;
 use crate::working_copy::CheckoutStats;
 use crate::working_copy::LockedWorkingCopy;
 use crate::working_copy::ResetError;
@@ -739,6 +738,9 @@ struct FsmonitorMatcher {
 /// Settings specific to the [`LocalWorkingCopy`] backend.
 #[derive(Clone, Debug, Default)]
 pub struct LocalWcSettings {
+    /// Conflict marker style to use when materializing files or when checking
+    /// changed files.
+    pub conflict_marker_style: ConflictMarkerStyle,
     /// Configuring auto-converting CRLF line endings into LF when you add a
     /// file to the backend, and vice versa when it checks out code onto your
     /// filesystem.
@@ -749,6 +751,7 @@ impl LocalWcSettings {
     /// Create an instance from [`UserSettings`].
     pub fn try_from_user_settings(user_settings: &UserSettings) -> Result<Self, ConfigGetError> {
         Ok(Self {
+            conflict_marker_style: user_settings.get("ui.conflict-marker-style")?,
             eol_conversion_mode: EolConversionMode::try_from_settings(user_settings)?,
         })
     }
@@ -757,6 +760,7 @@ impl LocalWcSettings {
 /// Configuration for the tree state.
 #[derive(Clone, Debug)]
 struct TreeStateConfig {
+    conflict_marker_style: ConflictMarkerStyle,
     symlink_support: bool,
     target_eol_strategy: TargetEolStrategy,
 }
@@ -765,6 +769,7 @@ impl TreeStateConfig {
     fn from_wc_settings(wc_settings: &LocalWcSettings) -> Self {
         Self {
             symlink_support: check_symlink_support().unwrap_or(false),
+            conflict_marker_style: wc_settings.conflict_marker_style,
             target_eol_strategy: TargetEolStrategy::new(wc_settings.eol_conversion_mode),
         }
     }
@@ -1028,7 +1033,6 @@ impl TreeState {
             progress,
             start_tracking_matcher,
             max_new_file_size,
-            conflict_marker_style,
         } = options;
 
         let sparse_matcher = self.sparse_matcher();
@@ -1070,7 +1074,6 @@ impl TreeState {
                 error: OnceLock::new(),
                 progress,
                 max_new_file_size,
-                conflict_marker_style,
             };
             let directory_to_visit = DirectoryToVisit {
                 dir: RepoPathBuf::root(),
@@ -1217,7 +1220,6 @@ struct FileSnapshotter<'a> {
     error: OnceLock<SnapshotError>,
     progress: Option<&'a SnapshotProgress<'a>>,
     max_new_file_size: u64,
-    conflict_marker_style: ConflictMarkerStyle,
 }
 
 impl FileSnapshotter<'_> {
@@ -1622,7 +1624,7 @@ impl FileSnapshotter<'_> {
                 self.store(),
                 repo_path,
                 &contents,
-                self.conflict_marker_style,
+                self.tree_state.config.conflict_marker_style,
                 materialized_conflict_data.map_or(MIN_CONFLICT_MARKER_LEN, |data| {
                     data.conflict_marker_len as usize
                 }),
@@ -1833,11 +1835,7 @@ impl TreeState {
         Ok(())
     }
 
-    pub fn check_out(
-        &mut self,
-        new_tree: &MergedTree,
-        options: &CheckoutOptions,
-    ) -> Result<CheckoutStats, CheckoutError> {
+    pub fn check_out(&mut self, new_tree: &MergedTree) -> Result<CheckoutStats, CheckoutError> {
         let old_tree = self.current_tree().map_err(|err| match err {
             err @ BackendError::ObjectNotFound { .. } => CheckoutError::SourceNotFound {
                 source: Box::new(err),
@@ -1845,12 +1843,7 @@ impl TreeState {
             other => CheckoutError::InternalBackendError(other),
         })?;
         let stats = self
-            .update(
-                &old_tree,
-                new_tree,
-                self.sparse_matcher().as_ref(),
-                options.conflict_marker_style,
-            )
+            .update(&old_tree, new_tree, self.sparse_matcher().as_ref())
             .block_on()?;
         self.tree_id = new_tree.id();
         Ok(stats)
@@ -1859,7 +1852,6 @@ impl TreeState {
     pub fn set_sparse_patterns(
         &mut self,
         sparse_patterns: Vec<RepoPathBuf>,
-        options: &CheckoutOptions,
     ) -> Result<CheckoutStats, CheckoutError> {
         let tree = self.current_tree().map_err(|err| match err {
             err @ BackendError::ObjectNotFound { .. } => CheckoutError::SourceNotFound {
@@ -1872,21 +1864,9 @@ impl TreeState {
         let added_matcher = DifferenceMatcher::new(&new_matcher, &old_matcher);
         let removed_matcher = DifferenceMatcher::new(&old_matcher, &new_matcher);
         let empty_tree = MergedTree::resolved(Tree::empty(self.store.clone(), RepoPathBuf::root()));
-        let added_stats = self
-            .update(
-                &empty_tree,
-                &tree,
-                &added_matcher,
-                options.conflict_marker_style,
-            )
-            .block_on()?;
+        let added_stats = self.update(&empty_tree, &tree, &added_matcher).block_on()?;
         let removed_stats = self
-            .update(
-                &tree,
-                &empty_tree,
-                &removed_matcher,
-                options.conflict_marker_style,
-            )
+            .update(&tree, &empty_tree, &removed_matcher)
             .block_on()?;
         self.sparse_patterns = sparse_patterns;
         assert_eq!(added_stats.updated_files, 0);
@@ -1907,7 +1887,6 @@ impl TreeState {
         old_tree: &MergedTree,
         new_tree: &MergedTree,
         matcher: &dyn Matcher,
-        conflict_marker_style: ConflictMarkerStyle,
     ) -> Result<CheckoutStats, CheckoutError> {
         // TODO: maybe it's better not include the skipped counts in the "intended"
         // counts
@@ -2008,7 +1987,7 @@ impl TreeState {
                         choose_materialized_conflict_marker_len(&file.contents);
                     let data = materialize_merge_result_to_bytes_with_marker_len(
                         &file.contents,
-                        conflict_marker_style,
+                        self.config.conflict_marker_style,
                         conflict_marker_len,
                     )
                     .into();
@@ -2437,11 +2416,7 @@ impl LockedWorkingCopy for LockedLocalWorkingCopy {
         Ok((tree_state.current_tree_id().clone(), stats))
     }
 
-    fn check_out(
-        &mut self,
-        commit: &Commit,
-        options: &CheckoutOptions,
-    ) -> Result<CheckoutStats, CheckoutError> {
+    fn check_out(&mut self, commit: &Commit) -> Result<CheckoutStats, CheckoutError> {
         // TODO: Write a "pending_checkout" file with the new TreeId so we can
         // continue an interrupted update if we find such a file.
         let new_tree = commit.tree()?;
@@ -2453,7 +2428,7 @@ impl LockedWorkingCopy for LockedLocalWorkingCopy {
                 err: err.into(),
             })?;
         if tree_state.tree_id != *commit.tree_id() {
-            let stats = tree_state.check_out(&new_tree, options)?;
+            let stats = tree_state.check_out(&new_tree)?;
             self.tree_state_dirty = true;
             Ok(stats)
         } else {
@@ -2500,7 +2475,6 @@ impl LockedWorkingCopy for LockedLocalWorkingCopy {
     fn set_sparse_patterns(
         &mut self,
         new_sparse_patterns: Vec<RepoPathBuf>,
-        options: &CheckoutOptions,
     ) -> Result<CheckoutStats, CheckoutError> {
         // TODO: Write a "pending_checkout" file with new sparse patterns so we can
         // continue an interrupted update if we find such a file.
@@ -2511,7 +2485,7 @@ impl LockedWorkingCopy for LockedLocalWorkingCopy {
                 message: "Failed to load the working copy state".to_string(),
                 err: err.into(),
             })?
-            .set_sparse_patterns(new_sparse_patterns, options)?;
+            .set_sparse_patterns(new_sparse_patterns)?;
         self.tree_state_dirty = true;
         Ok(stats)
     }
